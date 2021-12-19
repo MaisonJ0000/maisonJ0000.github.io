7a418e6da97f7f540e8b0e183a249dc80155fc22
MaisonJ0000 <maisonj0000@gmail.com>
2021-12-20 04:54:54 +0900

    add placeholder

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,21 @@ interface IProps {
   lastPosts: any[];
 }
 
+// Pixel GIF code adapted from https://stackoverflow.com/a/33919020/266535
+const keyStr =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+const triplet = (e1: any, e2: any, e3: any) =>
+  keyStr.charAt(e1 >> 2) +
+  keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+  keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+  keyStr.charAt(e3 & 63);
+
+const rgbDataURL = (r: number, g: number, b: number) =>
+  `data:image/gif;base64,R0lGODlhAQABAPAA${
+    triplet(0, r, g) + triplet(b, 255, 255)
+  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
+
 const Home = ({ lastPosts }: IProps) => {
   console.log('[COZE_LOG] lastPosts', lastPosts);
   return (
@@ -27,6 +42,7 @@ const Home = ({ lastPosts }: IProps) => {
               layout="responsive"
               objectFit="cover"
               placeholder="blur"
+              blurDataURL={rgbDataURL(120, 144, 80)}
               priority
             />
           </div>
